3fee93457ab8b7ad8947a0703f365dcf5faf1c47
rohannstu <rohannstu@gmail.com>
2026-01-13 00:56:44 +0600

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿class Animal
+{
+    public virtual void Speak()
+    {
+        System.Console.WriteLine("The animal makes a sound.");
+    }
+    public virtual void Flying () 
+    {
+        System.Console.WriteLine("The animal is flying.");
+    }
+}
+
+class Penguin : Animal
+{
+    public override void Speak()
+    {
+        System.Console.WriteLine("The penguin makes a sound.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        myAnimal.Speak(); // Output: The animal makes a sound.
+        myAnimal.Flying(); // Output: The animal is flying.
+
+        Penguin myPenguin = new Penguin();
+        myPenguin.Speak(); // Output: The penguin makes a sound.
+        myPenguin.Flying(); // Output: The animal is flying.
+    }
+}
