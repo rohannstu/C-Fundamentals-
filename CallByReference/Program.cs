fa7feea8902b29f166d41e2136b0ef32a284e83a
rohannstu <rohannstu@gmail.com>
2026-01-13 00:34:20 +0600

    testing

---
+++ CallByReference/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class CallByRef
+{
+    static void Main()
+    {
+        Employee emp1 = new()
+        {
+            EmployeeID = 1001,
+            Name = "James"
+        };
+
+        Employee emp2 = emp1;
+
+        emp1.Name = "Smith";
+
+        Console.WriteLine($"Emp1 Name = {emp1.Name}");
+        Console.WriteLine($"Emp2 Name = {emp2.Name}");
+    }
+}
+
+public class Employee
+{
+    public int EmployeeID { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
---
+++ CallByValue/Program.cs
@@ -0,0 +1,5 @@
+﻿int a = 10; 
+int b = a; 
+b = 20;
+Console.WriteLine($"a = {a}"); // Outputs: a = 10
+Console.WriteLine($"b = {b}"); // Outputs: b = 20
